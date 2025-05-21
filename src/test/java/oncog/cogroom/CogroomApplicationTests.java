f1c5660eef7585717eed1a4b89a5642f33a11e51
Son Hyeok <87377014+SonHyeok@users.noreply.github.com>
2025-05-21 23:16:42 +0900

    [FEAT] 템플릿 및 환경 변수 설정
    issue/pr template, DB/S3 Infra setting

---
+++ src/test/java/oncog/cogroom/CogroomApplicationTests.java
@@ -0,0 +1,13 @@
+package oncog.cogroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CogroomApplicationTests {
+
+//	@Test
+	void contextLoads() {
+	}
+
+}
