4bb2d462d2e1bb80e5697db51dcabfcae3b2751b
Son Hyeok <thsgur1212@gmail.com>
2025-05-21 22:59:20 +0900

    feat: issue/pr 템플릿 생성

---
+++ src/main/java/oncog/cogroom/CogroomApplication.java
@@ -0,0 +1,12 @@
+package oncog.cogroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CogroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CogroomApplication.class, args);
+	}
+}
