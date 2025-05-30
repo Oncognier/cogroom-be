d5f841c588be8e4f6777f05934d4a73f3f75805e
Son Hyeok <thsgur1212@gmail.com>
2025-05-30 19:58:07 +0900

    feat: 인증 링크 이메일 전송 API 구현

--- src/main/java/oncog/cogroom/domain/auth/controller/OAuthController.java
+++ src/main/java/oncog/cogroom/domain/auth/controller/OAuthController.java
@@ -1,12 +1,17 @@
 package oncog.cogroom.domain.auth.controller;
 
+import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import oncog.cogroom.domain.auth.dto.response.SocialResponseDTO;
+import oncog.cogroom.domain.auth.service.EmailService;
 import oncog.cogroom.domain.auth.service.OAuthServiceRouter;
 import oncog.cogroom.domain.member.enums.Provider;
+import oncog.cogroom.global.common.response.apiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @RestController
 @Slf4j
@@ -15,14 +20,29 @@ import org.springframework.web.bind.annotation.*;
 public class OAuthController {
 
     private final OAuthServiceRouter router;
+    private final EmailService emailService;
+
     @PostMapping("/social-login")
-    public ResponseEntity<SocialResponseDTO.LoginResponseDTO> socialLogin(@RequestParam Provider provider,
+    public ResponseEntity<apiResponse<SocialResponseDTO.LoginResponseDTO>> socialLogin(@RequestParam Provider provider,
                                                                           @RequestParam String authorizationCode) {
         SocialResponseDTO.LoginResponseDTO responseDTO = router.login(provider, authorizationCode);
 
-        return ResponseEntity.ok(responseDTO);
+        return ResponseEntity.ok(apiResponse.success(responseDTO));
     }
 
+
+    @PostMapping("/send-verification")
+    public ResponseEntity<apiResponse<String>> sendEmail(@RequestParam String userEmail) throws MessagingException, IOException {
+        emailService.sendEmail(userEmail);
+
+        return ResponseEntity.ok(apiResponse.success());
+    }
+
+    @GetMapping("/check-verification")
+    public ResponseEntity<apiResponse<Void>> verifyEmail(@RequestParam String verificationCode) {
+        return ResponseEntity.ok(apiResponse.success());
+
+    }
     // 인가 코드 반환받을 테스트 컨트롤러
     @GetMapping("/login/code")
     public ResponseEntity<String> test(@RequestParam String code) {
--- src/main/java/oncog/cogroom/domain/auth/service/EmailService.java
+++ src/main/java/oncog/cogroom/domain/auth/service/EmailService.java
@@ -1,11 +1,63 @@
 package oncog.cogroom.domain.auth.service;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import oncog.cogroom.domain.auth.entity.EmailVerification;
+import oncog.cogroom.domain.auth.repository.EmailRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import java.security.SecureRandom;
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 public class EmailService {
+    private final EmailRepository emailRepository;
+    private final JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
+    @Value("${spring.mail.email-link-url}")
+    private String emailLinkUrl;
+
+    public void sendEmail(String toEmail) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, "UTF-8");
+
+        helper.setTo(toEmail); // 목적지
+        helper.setSubject(generateVerificationLink(toEmail)); // 타이틀
+        helper.setFrom(fromEmail); // 발신 이메일
+        mailSender.send(message);
+
+    }
+
+    private void saveEmail(String toEmail, int verificationCode) {
+        emailRepository.save(EmailVerification.builder()
+                .email(toEmail)
+                .verifyCode(String.valueOf(verificationCode))
+                .verifyStatus(false)
+                .expireDate(LocalDateTime.now().plusMinutes(10))
+                .build());
+    }
+
+    // 인증 링크 생성 및 이메일 인증 객체 저장
+    private String generateVerificationLink(String toEmail) {
+        int verificationCode = generateVerificationCode();
+
+        saveEmail(toEmail, verificationCode);
+
+        return String.format("%s/api/v1/auth/%s 링크를 10분 이내에 클릭해주세요.", emailLinkUrl, verificationCode);
+    }
 
 
+    // 인증 코드 생성
+    private int generateVerificationCode() {
+        SecureRandom secureRandom = new SecureRandom();
+        return 100000 + secureRandom.nextInt(900000);
+    }
 }
