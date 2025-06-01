217eb2319b30abe15903493eae33497f46035180
hikhjin <hikhjin@gmail.com>
2025-06-01 22:51:32 +0900

    feat: 스트릭 기록 초기화 스케줄러 추가

---
+++ src/main/java/oncog/cogroom/domain/streak/scheduler/StreakSchduler.java
@@ -0,0 +1,23 @@
+package oncog.cogroom.domain.streak.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import oncog.cogroom.domain.streak.service.StreakService;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class StreakSchduler {
+
+    private final StreakService streakService;
+
+    @Scheduled(cron = "0 0 0 * * *") // 자정마다
+//    @Scheduled(cron = "0 */2 * * * *") // 2분마다, 테스트용
+    public void updateStreakAtMidnight() {
+        log.info("스트릭 기록 초기화 시작");
+        streakService.updateAllMemberStreaks();
+        log.info("스트릭 기록 초기화 완료");
+    }
+}
