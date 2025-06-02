99598b38af7a79c125b2368ed974cf131b1d403a
hikhjin <hikhjin@gmail.com>
2025-06-03 02:16:49 +0900

    refactor: 응답 형식 변경

--- src/main/java/oncog/cogroom/global/common/response/ApiErrorResponse.java
+++ src/main/java/oncog/cogroom/global/common/response/ApiErrorResponse.java
@@ -2,26 +2,15 @@ package oncog.cogroom.global.common.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import oncog.cogroom.global.common.response.code.ApiErrorCode;
 
 @Getter
 @AllArgsConstructor
 public class ApiErrorResponse {
-    private final Integer code;
+    private final String code;
     private final String message;
 
-    public static ApiErrorResponse badRequest(String detail) {
-        return new ApiErrorResponse(400, "잘못된 요청입니다.");
-    }
-
-    public static ApiErrorResponse unauthorized(String detail) {
-        return new ApiErrorResponse(401, "인증이 필요합니다.");
-    }
-
-    public static ApiErrorResponse forbidden(String detail) {
-        return new ApiErrorResponse(403, "접근이 거부되었습니다.");
-    }
-
-    public static ApiErrorResponse serverError(String detail) {
-        return new ApiErrorResponse(500, "서버 오류가 발생했습니다.");
+    public static <T> ApiResponse<T> success(ApiErrorCode code) {
+        return new ApiResponse<>(code.name(), code.getMessage(), null);
     }
 }
