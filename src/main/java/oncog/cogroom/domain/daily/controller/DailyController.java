4d7c6715e844f9b4ce9517f5c20f457b69e2a6a2
hikhjin <hikhjin@gmail.com>
2025-06-02 16:37:00 +0900

    feat: daily 질문 조회 컨트롤러 추가

---
+++ src/main/java/oncog/cogroom/domain/daily/controller/DailyController.java
@@ -0,0 +1,38 @@
+package oncog.cogroom.domain.daily.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import oncog.cogroom.domain.daily.dto.response.DailyQuestionResponse;
+import oncog.cogroom.domain.daily.service.DailyService;
+import oncog.cogroom.global.common.response.ApiResponse;
+import oncog.cogroom.global.security.domain.CustomUserDetails;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/daily")
+public class DailyController {
+
+    private final DailyService dailyService;
+
+    @GetMapping("/questions")
+    public ApiResponse<DailyQuestionResponse> getDailyQuestion() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            throw new RuntimeException("로그인이 필요합니다.");
+        }
+
+        CustomUserDetails user = (CustomUserDetails) authentication.getPrincipal();
+
+        DailyQuestionResponse response = dailyService.getTodayDailyQuestion(user.getMemberId());
+
+        return ApiResponse.success(response);
+    }
+
+
+}
