f97a24a96bdf6b2474610c12cf4317877203e92c
hikhjin <hikhjin@gmail.com>
2025-06-02 16:38:25 +0900

    feat: memberId로 streak 조회 메서드 추가

--- src/main/java/oncog/cogroom/domain/streak/repository/StreakRepository.java
+++ src/main/java/oncog/cogroom/domain/streak/repository/StreakRepository.java
@@ -3,5 +3,8 @@ package oncog.cogroom.domain.streak.repository;
 import oncog.cogroom.domain.streak.entity.Streak;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StreakRepository extends JpaRepository<Streak, Long> {
+    Optional<Streak> findByMemberId(Long memberId);
 }
