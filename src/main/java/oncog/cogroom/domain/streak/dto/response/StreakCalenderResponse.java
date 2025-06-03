c1c3a9459bf6455c93a99a2b36a070b8dfc76b7c
hikhjin <hikhjin@gmail.com>
2025-06-03 23:58:56 +0900

    feat: 물방울(스트릭) 조회 dto 추가

---
+++ src/main/java/oncog/cogroom/domain/streak/dto/response/StreakCalenderResponse.java
@@ -0,0 +1,14 @@
+package oncog.cogroom.domain.streak.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class StreakCalenderResponse {
+    private List<String> streakDateList;
+}
