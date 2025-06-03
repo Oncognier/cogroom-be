bb1bde1f63a2d1bdd129890f4898c73893ff3748
Son Hyeok <thsgur1212@gmail.com>
2025-06-04 02:34:08 +0900

    refactor: jwt & 카카오 로그인 에러 처리

--- src/main/java/oncog/cogroom/domain/auth/service/social/KakaoAuthService.java
+++ src/main/java/oncog/cogroom/domain/auth/service/social/KakaoAuthService.java
@@ -2,22 +2,23 @@ package oncog.cogroom.domain.auth.service.social;
 
 import lombok.extern.slf4j.Slf4j;
 import oncog.cogroom.domain.auth.dto.response.SocialTokenResponseDTO;
-import oncog.cogroom.domain.auth.dto.response.SocialUserInfoDTO;
 import oncog.cogroom.domain.auth.userInfo.KakaoUserInfo;
 import oncog.cogroom.domain.auth.userInfo.SocialUserInfo;
 import oncog.cogroom.domain.member.enums.Provider;
 import oncog.cogroom.domain.member.repository.MemberRepository;
+import oncog.cogroom.global.common.response.code.ApiErrorCode;
 import oncog.cogroom.global.common.util.TokenUtil;
-import oncog.cogroom.global.security.jwt.JwtProvider;
+import oncog.cogroom.global.exception.domain.AuthException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
-import static oncog.cogroom.domain.auth.dto.response.SocialUserInfoDTO.*;
+import static oncog.cogroom.domain.auth.dto.response.SocialUserInfoDTO.KakaoUserInfoDTO;
 
 @Service
 @Slf4j
@@ -35,27 +36,32 @@ public class KakaoAuthService extends AbstractSocialAuthService {
     // 카카오 액세스 토큰 조회
     @Override
     protected String requestAccessToken(String code) {
-       HttpEntity<MultiValueMap<String, String>> request = getHttpEntityForToken(code);
+        try {
+            HttpEntity<MultiValueMap<String, String>> request = getHttpEntityForToken(code);
 
-        ResponseEntity<SocialTokenResponseDTO.KakaoTokenDTO> response = restTemplate.exchange(
-                "https://kauth.kakao.com/oauth/token",
-                HttpMethod.POST,
-                request,
-                SocialTokenResponseDTO.KakaoTokenDTO.class
-        );
+            ResponseEntity<SocialTokenResponseDTO.KakaoTokenDTO> response = restTemplate.exchange(
+                    "https://kauth.kakao.com/oauth/token",
+                    HttpMethod.POST,
+                    request,
+                    SocialTokenResponseDTO.KakaoTokenDTO.class
+            );
 
-        log.info("카카오 토큰 응답: {}", response.getBody());
+            log.info("카카오 토큰 응답: {}", response.getBody());
 
-        return response.getBody().getAccessToken();
+            return response.getBody().getAccessToken();
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            log.error("카카오 액세스 토큰 요청 실패 : {}", e.getResponseBodyAsString());
+            throw new AuthException(ApiErrorCode.KAKAO_AUTH_FAILED);
+        }
     }
 
     // 카카오 사용자 정보 조회
     @Override
     protected SocialUserInfo requestUserInfo(String accessToken) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setBearerAuth(accessToken);
-
         try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.setBearerAuth(accessToken);
+
             KakaoUserInfoDTO responseDTO = restTemplate.postForEntity(
                     "https://kapi.kakao.com/v2/user/me",
                     new HttpEntity<>(headers),
@@ -66,7 +72,7 @@ public class KakaoAuthService extends AbstractSocialAuthService {
 
         } catch (HttpClientErrorException e) {
             log.error("Kakao 사용자 정보 조회 API 호출 실패: 상태 코드 {}, 응답 본문 {}", e.getStatusCode(), e.getResponseBodyAsString());
-            throw new IllegalArgumentException("not found");
+            throw new AuthException(ApiErrorCode.KAKAO_AUTH_FAILED);
         }
     }
 
