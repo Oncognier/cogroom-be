fc4ff55fd6cf911ff925d7e91a528d73980ca2d9
hikhjin <hikhjin@gmail.com>
2025-06-04 15:07:45 +0900

    feat: StreakService 물방울(스트릭) 조회 기능 추가

--- src/main/java/oncog/cogroom/domain/streak/service/StreakService.java
+++ src/main/java/oncog/cogroom/domain/streak/service/StreakService.java
@@ -5,10 +5,14 @@ import lombok.extern.slf4j.Slf4j;
 import oncog.cogroom.domain.streak.entity.Streak;
 import oncog.cogroom.domain.streak.repository.StreakLogRepository;
 import oncog.cogroom.domain.streak.repository.StreakRepository;
+import oncog.cogroom.domain.streak.dto.response.StreakCalenderResponse;
+import oncog.cogroom.global.security.jwt.JwtProvider;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 
@@ -19,6 +23,8 @@ public class StreakService {
 
     private final StreakRepository streakRepository;
     private final StreakLogRepository streakLogRepository;
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private final JwtProvider jwtProvider;
 
     // 특정 멤버의 스트릭 기록 초기화
     @Transactional
@@ -43,5 +49,27 @@ public class StreakService {
                streak.resetTotalDays();
            }
         });
+    }
+
+    public StreakCalenderResponse getStreakDates() {
+        Long memberId = jwtProvider.extractMemberId();
+
+        LocalDate today = LocalDate.now();
+        LocalDate firstDayOfMonth = today.withDayOfMonth(1);
+        LocalDate lastDayOfMonth = today.withDayOfMonth(today.lengthOfMonth());
+
+        LocalDateTime startOfMonth = firstDayOfMonth.atStartOfDay();
+        LocalDateTime endOfMonth = lastDayOfMonth.atTime(23, 59, 59);
+
+        List<String> streakDates = streakLogRepository
+                .findAllByMemberIdAndCreatedAtBetween(memberId, startOfMonth, endOfMonth).stream()
+                .map(log -> log.getCreatedAt().toLocalDate().format(formatter))
+                .distinct()
+                .sorted()
+                .toList();
+
+        return StreakCalenderResponse.builder()
+                .streakDateList(streakDates)
+                .build();
     }
 }
