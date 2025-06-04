94312bfd4985a509f2cdffc8a4e688519945eb18
hikhjin <hikhjin@gmail.com>
2025-06-04 15:06:27 +0900

    feat: streak controller 추가

---
+++ src/main/java/oncog/cogroom/domain/streak/controller/StreakController.java
@@ -0,0 +1,28 @@
+package oncog.cogroom.domain.streak.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import oncog.cogroom.domain.streak.dto.response.StreakCalenderResponse;
+import oncog.cogroom.domain.streak.service.StreakService;
+import oncog.cogroom.global.common.response.ApiResponse;
+import oncog.cogroom.global.common.response.code.ApiSuccessCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/streaks")
+public class StreakController {
+
+    private final StreakService streakService;
+
+    @GetMapping("/calender")
+    public ResponseEntity<ApiResponse<StreakCalenderResponse>> getStreakCalender() {
+        StreakCalenderResponse response = streakService.getStreakDates();
+
+        return ResponseEntity.ok(ApiResponse.of(ApiSuccessCode.SUCCESS, response));
+    }
+}
