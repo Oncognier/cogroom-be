b71af31dfa853e1818474bf251e06b6194128eb1
hikhjin <hikhjin@gmail.com>
2025-06-04 15:07:22 +0900

    feat: StreakLogRepository 메서드 추가

--- src/main/java/oncog/cogroom/domain/streak/repository/StreakLogRepository.java
+++ src/main/java/oncog/cogroom/domain/streak/repository/StreakLogRepository.java
@@ -4,7 +4,10 @@ import oncog.cogroom.domain.streak.entity.StreakLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public interface StreakLogRepository extends JpaRepository<StreakLog, Long> {
     boolean existsByMemberIdAndCreatedAtBetween(Long memberId, LocalDateTime start, LocalDateTime end);
+
+    List<StreakLog> findAllByMemberIdAndCreatedAtBetween(Long memberId, LocalDateTime start, LocalDateTime end);
 }
