7019bd0df023603054574bb067818410b67ff676
hikhjin <hikhjin@gmail.com>
2025-06-07 01:38:23 +0900

    feat: member, question으로 answer 조회 메서드 추가

--- src/main/java/oncog/cogroom/domain/daily/respository/AnswerRepository.java
+++ src/main/java/oncog/cogroom/domain/daily/respository/AnswerRepository.java
@@ -1,6 +1,8 @@
 package oncog.cogroom.domain.daily.respository;
 
 import oncog.cogroom.domain.daily.entity.Answer;
+import oncog.cogroom.domain.daily.entity.Question;
+import oncog.cogroom.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -15,4 +17,6 @@ public interface AnswerRepository extends JpaRepository<Answer, Long> {
         AND a.createdAt BETWEEN :start AND :end
     """)
     Optional<String> findByMemberAndCreatedAtBetween(Long id, LocalDateTime start, LocalDateTime end);
+
+    boolean existsByMemberAndQuestion(Member member, Question question);
 }
