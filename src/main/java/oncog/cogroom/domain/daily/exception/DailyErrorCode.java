42308e40e7be6fee0e2ef41cf38e91b83eeb53d7
hikhjin <hikhjin@gmail.com>
2025-06-07 01:38:36 +0900

    feat: 데일리 에러코드 추가

--- src/main/java/oncog/cogroom/domain/daily/exception/DailyErrorCode.java
+++ src/main/java/oncog/cogroom/domain/daily/exception/DailyErrorCode.java
@@ -9,7 +9,10 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum DailyErrorCode implements BaseErrorCode {
 
-    DAILY_QUESTION_NOT_FOUND("DAILY_QUESTION_NOT_FOUND", HttpStatus.NOT_FOUND, "데일리 질문을 찾을 수 없습니다.");
+    DAILY_QUESTION_NOT_FOUND("DAILY_QUESTION_NOT_FOUND", HttpStatus.NOT_FOUND, "데일리 질문을 찾을 수 없습니다."),
+    QUESTION_NOT_FOUND("QUESTION_NOT_FOUND", HttpStatus.NOT_FOUND, "해당 질문을 찾을 수 없습니다."),
+    INVALID_QUESTION("INVALID_QUESTION", HttpStatus.BAD_REQUEST, "오늘 할당된 데일리 질문이 아닙니다."),
+    ALREADY_ANSWERED("ALREADY_ANSWERED", HttpStatus.CONFLICT, "이미 답변이 존재합니다.");
 
     private final String code;
     private final HttpStatus status;
