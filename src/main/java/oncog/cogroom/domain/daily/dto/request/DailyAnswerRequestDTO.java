cc979a7b6a6a1f9fc8110e50dee6095d4cbcbcec
hikhjin <hikhjin@gmail.com>
2025-06-08 00:32:27 +0900

    refactor: answer dto 수정

--- src/main/java/oncog/cogroom/domain/daily/dto/request/DailyAnswerRequestDTO.java
+++ src/main/java/oncog/cogroom/domain/daily/dto/request/DailyAnswerRequestDTO.java
@@ -1,15 +1,12 @@
 package oncog.cogroom.domain.daily.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class DailyAnswerRequestDTO {
-    private Long questionId;
-
     @NotBlank
     private String answer;
 }
