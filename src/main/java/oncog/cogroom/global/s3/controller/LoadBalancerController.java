9bfde5b0d4b1641861312a55b778989e7f726ccc
Son Hyeok <thsgur1212@gmail.com>
2025-06-09 00:30:16 +0900

    feat: healthCheck API 구현

---
+++ src/main/java/oncog/cogroom/global/s3/controller/LoadBalancerController.java
@@ -0,0 +1,16 @@
+package oncog.cogroom.global.s3.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoadBalancerController {
+
+    @GetMapping("/healthcheck")
+    public ResponseEntity<Void> healthCheck() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
