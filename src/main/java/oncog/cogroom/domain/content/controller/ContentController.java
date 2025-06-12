d946acd1690f05c1af33b56eab2df00f28f97b70
hikhjin <hikhjin@gmail.com>
2025-06-12 16:30:12 +0900

    feat: 콘텐츠 목록 조회 API 구현

---
+++ src/main/java/oncog/cogroom/domain/content/controller/ContentController.java
@@ -0,0 +1,32 @@
+package oncog.cogroom.domain.content.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import oncog.cogroom.domain.content.dto.ContentResponseDTO;
+import oncog.cogroom.domain.content.service.ContentService;
+import oncog.cogroom.global.common.response.ApiResponse;
+import oncog.cogroom.global.common.response.code.ApiSuccessCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/contents")
+public class ContentController {
+
+    private final ContentService contentService;
+
+    @GetMapping("/home")
+    public ResponseEntity<ApiResponse<List<ContentResponseDTO>>> getHomeContents() {
+        List<ContentResponseDTO> contentList = contentService.getHomeContents();
+
+        return ResponseEntity.ok(ApiResponse.of(ApiSuccessCode.SUCCESS, contentList));
+    }
+}
---
+++ src/main/java/oncog/cogroom/domain/content/service/ContentService.java
@@ -0,0 +1,55 @@
+package oncog.cogroom.domain.content.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import oncog.cogroom.domain.content.dto.ContentResponseDTO;
+import oncog.cogroom.domain.content.entity.Content;
+import oncog.cogroom.domain.content.entity.ContentImage;
+import oncog.cogroom.domain.content.exception.ContentErrorCode;
+import oncog.cogroom.domain.content.exception.ContentException;
+import oncog.cogroom.domain.content.repository.ContentImageRepository;
+import oncog.cogroom.domain.content.repository.ContentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ContentService {
+
+    private static final int THUMBNAIL_IMAGE_DISPLAY_ORDER = 1;
+
+    private final ContentRepository contentRepository;
+    private final ContentImageRepository contentImageRepository;
+
+    public List<ContentResponseDTO> getHomeContents() {
+        List<Content> contents = getContents();
+
+        return contents.stream()
+                .map(content -> ContentResponseDTO.builder()
+                        .id(content.getId())
+                        .title(content.getName())
+                        .imageUrl(getContentImage(content).getImageUrl())
+                        .summary(content.getSummary())
+                        .build())
+                .collect(Collectors.toList());
+    }
+
+    private ContentImage getContentImage(Content content) {
+        return contentImageRepository.findByContentAndDisplayOrderIs(content, THUMBNAIL_IMAGE_DISPLAY_ORDER)
+                .orElseThrow(() -> new ContentException(ContentErrorCode.IMAGE_NOT_FOUND));
+    }
+
+    private List<Content> getContents() {
+        List<Content> contents = contentRepository.findAll();
+
+        if (contents.isEmpty()) {
+            throw new ContentException(ContentErrorCode.CONTENTS_NOT_FOUND);
+        }
+
+        return contents;
+    }
+
+}
