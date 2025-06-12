c70d0b51bb3473b664ee5a91f9cc0b1304a8dd70
hikhjin <hikhjin@gmail.com>
2025-06-12 22:39:38 +0900

    refactor: ONSALE인 콘텐츠만 조회하는 메서드 추가

--- src/main/java/oncog/cogroom/domain/content/repository/ContentRepository.java
+++ src/main/java/oncog/cogroom/domain/content/repository/ContentRepository.java
@@ -1,7 +1,11 @@
 package oncog.cogroom.domain.content.repository;
 
 import oncog.cogroom.domain.content.entity.Content;
+import oncog.cogroom.domain.content.enums.ContentStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ContentRepository extends JpaRepository<Content, Long> {
+    List<Content> findByStatus(ContentStatus status);
 }
