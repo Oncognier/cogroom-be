433645877c624b629951e1b58612254438800cfe
Son Hyeok <thsgur1212@gmail.com>
2025-06-14 00:20:26 +0900

    refactor: 토큰 갱신, 로그아웃 스웨거 수정

--- src/main/java/oncog/cogroom/domain/auth/controller/docs/AuthControllerDocs.java
+++ src/main/java/oncog/cogroom/domain/auth/controller/docs/AuthControllerDocs.java
@@ -3,11 +3,13 @@ package oncog.cogroom.domain.auth.controller.docs;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.mail.MessagingException;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import oncog.cogroom.domain.auth.dto.request.AuthRequestDTO;
 import oncog.cogroom.domain.auth.dto.response.AuthResponseDTO;
 import oncog.cogroom.domain.auth.exception.AuthErrorCode;
+import oncog.cogroom.domain.member.exception.MemberErrorCode;
 import oncog.cogroom.global.common.response.ApiResponse;
 import oncog.cogroom.global.common.response.code.ApiErrorCode;
 import oncog.cogroom.global.common.response.code.ApiSuccessCode;
@@ -53,7 +55,18 @@ public interface AuthControllerDocs {
     @Operation(summary = "이메일 인증 여부 반환", description = "이메일의 인증이 완료되었는지 여부를 반환합니다.")
     public ResponseEntity<ApiResponse<Boolean>> checkEmailVerificationStatus(@RequestBody @Valid AuthRequestDTO.EmailRequestDTO request);
 
+    @ApiErrorCodeExamples(
+            value = {AuthErrorCode.class, MemberErrorCode.class},
+            include = {"INVALID_TOKEN", "IN_BLACK_LIST", "EXPIRED_TOKEN", "MEMBER_NOT_FOUND"}
+    )
     @Operation(summary = "토큰 갱신 API", description = "토큰 재발급 API 입니다.")
     public ResponseEntity<ApiResponse<Void>> reIssue(@CookieValue String refreshToken,
                                                      HttpServletResponse response);
+
+    @ApiErrorCodeExamples(
+            value = {AuthErrorCode.class, MemberErrorCode.class},
+            include = {"INVALID_TOKEN", "IN_BLACK_LIST", "EXPIRED_TOKEN", "MEMBER_NOT_FOUND"}
+    )
+    @Operation(summary = "로그아웃 API", description = "로그아웃 API 입니다.")
+    public ResponseEntity<ApiResponse<Void>> logout(HttpServletRequest request);
 }
