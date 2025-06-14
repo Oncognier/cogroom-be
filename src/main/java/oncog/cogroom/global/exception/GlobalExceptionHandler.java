18917bce30008105d1ea64c43aa45b14c5d5b7d5
Son Hyeok <thsgur1212@gmail.com>
2025-06-14 19:13:38 +0900

    refactor: 에러 커스텀 핸들러 리팩토링

--- src/main/java/oncog/cogroom/global/exception/GlobalExceptionHandler.java
+++ src/main/java/oncog/cogroom/global/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import oncog.cogroom.domain.daily.exception.DailyErrorCode;
 import oncog.cogroom.global.common.response.ApiErrorResponse;
 import oncog.cogroom.global.common.response.code.ApiErrorCode;
 import oncog.cogroom.global.common.response.code.BaseErrorCode;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 @Slf4j
 @RestControllerAdvice
@@ -40,11 +42,7 @@ public class GlobalExceptionHandler {
     // 어노테이션 유효성 체크 핸들러
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ApiErrorResponse> handleArgumentException(MethodArgumentNotValidException e) {
-        FieldError fieldError = e.getBindingResult().getFieldErrors().get(0);
-
-        BaseErrorCode baseErrorCode = mapFieldErrorToErrorCode(fieldError);
-
-        return buildErrorResponse(baseErrorCode);
+        return buildErrorResponse(mapFieldErrorToErrorCode(e.getFieldError()));
     }
 
     // 공통 ResponseEntity
@@ -54,42 +52,24 @@ public class GlobalExceptionHandler {
                 .body(ApiErrorResponse.of(errorCode));
     }
 
-    private BaseErrorCode mapFieldErrorToErrorCode(FieldError error) {
+    // 어노테이션 유효성 체크용 errorResponse 응답 반환하도록 오버로딩
+    private ResponseEntity<ApiErrorResponse> buildErrorResponse(ApiErrorResponse errorResponse) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(errorResponse);
+    }
+
+    // 어노테이션 유효성 체크 응답 생성
+    private ApiErrorResponse mapFieldErrorToErrorCode(FieldError error) {
+        String message =  error.getDefaultMessage();
+
+        return ApiErrorResponse.of(generateErrorCode(error), message);
+    }
+
+    private String generateErrorCode(FieldError error) {
         String field = error.getField();
         String code = error.getCode();
-        String message = error.getDefaultMessage();
-
-        // 형식 관련 오류
-        if ("Email".equals(code)) {
-            return AuthErrorCode.INVALID_EMAIL_FORMAT;
-        }
-
-        if ("Pattern".equals(code)) {
-            switch (field) {
-                case "password" -> {
-                    return  AuthErrorCode.INVALID_PASSWORD_FORMAT;
-                }
-                case "phoneNumber" -> {
-                    return AuthErrorCode.INVALID_PHONE_NUMBER_PATTERN;
-                }
-                default -> {
-                    return ApiErrorCode.INVALID_PATTERN;
-                }
-            }
-        }
-
-        // 필수 값 누락
-        if ("NotBlank".equals(code) || "NotNull".equals(code)) {
-            return ApiErrorCode.EMPTY_FILED;
-        }
-
-        // 글자 수 제한 초과
-        if ("Size".equals(code) && "answer".equals(field) && "answerSizeExceeded".equals(message)) {
-            return DailyErrorCode.ANSWER_LENGTH_EXCEEDED;
-        }
-
-        // 기타 처리되지 않은 예외
-        return ApiErrorCode.BAD_REQUEST;
+        return (field + "_" + code + "_" + "error").toUpperCase();
     }
 
 
