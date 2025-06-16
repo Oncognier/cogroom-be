ae223a0b667b59b1b3e3e651346f2fa14cc66acb
김현진 <55572372+hikhjin@users.noreply.github.com>
2025-06-16 23:29:28 +0900

    refactor: 에러코드 및 스웨거 수정 #93 (#104)

--- src/main/java/oncog/cogroom/domain/streak/controller/docs/StreakControllerDocs.java
+++ src/main/java/oncog/cogroom/domain/streak/controller/docs/StreakControllerDocs.java
@@ -2,6 +2,7 @@ package oncog.cogroom.domain.streak.controller.docs;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import oncog.cogroom.domain.auth.exception.AuthErrorCode;
 import oncog.cogroom.domain.member.exception.MemberErrorCode;
 import oncog.cogroom.domain.streak.dto.response.StreakCalendarResponseDTO;
 import oncog.cogroom.global.common.response.ApiResponse;
@@ -14,7 +15,7 @@ public interface StreakControllerDocs {
 
     @Operation(summary = "물방울(스트릭) 기록 조회", description = "물방울 기록을 리스트로 반환합니다.")
     @ApiErrorCodeExamples(
-            value = {MemberErrorCode.class, ApiErrorCode.class},
-            include = {"MEMBER_NOT_FOUND_ERROR", "INTERNAL_SERVER_ERROR"})
+            value = {MemberErrorCode.class, AuthErrorCode.class, ApiErrorCode.class},
+            include = {"MEMBER_NOT_FOUND_ERROR", "TOKEN_INVALID_ERROR", "TOKEN_EXPIRED_ERROR", "INTERNAL_SERVER_ERROR"})
     ResponseEntity<ApiResponse<StreakCalendarResponseDTO>> getStreakCalendar();
 }
