4702bc034dc85f62b514ee8ca6430eabcb31d76b
hikhjin <hikhjin@gmail.com>
2025-06-18 23:38:03 +0900

    feat: 데일리 스트릭 조회 기능 추가

--- src/main/java/oncog/cogroom/domain/streak/controller/docs/StreakControllerDocs.java
+++ src/main/java/oncog/cogroom/domain/streak/controller/docs/StreakControllerDocs.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import oncog.cogroom.domain.auth.exception.AuthErrorCode;
 import oncog.cogroom.domain.member.exception.MemberErrorCode;
-import oncog.cogroom.domain.streak.dto.response.StreakCalendarResponseDTO;
+import static oncog.cogroom.domain.streak.dto.StreakResponseDTO.*;
 import oncog.cogroom.global.common.response.ApiResponse;
 import oncog.cogroom.global.common.response.code.ApiErrorCode;
 import oncog.cogroom.global.exception.swagger.ApiErrorCodeExamples;
@@ -17,5 +17,11 @@ public interface StreakControllerDocs {
     @ApiErrorCodeExamples(
             value = {MemberErrorCode.class, AuthErrorCode.class, ApiErrorCode.class},
             include = {"MEMBER_NOT_FOUND_ERROR", "TOKEN_INVALID_ERROR", "TOKEN_EXPIRED_ERROR", "INTERNAL_SERVER_ERROR"})
-    ResponseEntity<ApiResponse<StreakCalendarResponseDTO>> getStreakCalendar();
+    ResponseEntity<ApiResponse<StreakCalendarDTO>> getStreakCalendarWithDailyStreak();
+
+    @Operation(summary = "데일리 스트릭 연속 일수 조회", description = "데일리 스트릭 연속 일수를 반환합니다.")
+    @ApiErrorCodeExamples(
+            value = {MemberErrorCode.class, AuthErrorCode.class, ApiErrorCode.class},
+            include = {"MEMBER_NOT_FOUND_ERROR", "TOKEN_INVALID_ERROR", "TOKEN_EXPIRED_ERROR", "INTERNAL_SERVER_ERROR"})
+    ResponseEntity<ApiResponse<DailyStreakDTO>> getDailyStreak();
 }
