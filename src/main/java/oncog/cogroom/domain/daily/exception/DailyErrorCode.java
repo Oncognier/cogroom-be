74c0e61b8cbcb68bfc6f3397942d0605d1988bd0
김현진 <hikhjin@gmail.com>
2025-07-01 11:17:05 +0900

    refactor: 에러 코드 잘못 삭제 복구

--- src/main/java/oncog/cogroom/domain/daily/exception/DailyErrorCode.java
+++ src/main/java/oncog/cogroom/domain/daily/exception/DailyErrorCode.java
@@ -14,6 +14,7 @@ public enum DailyErrorCode implements BaseErrorCode {
     ASSIGNED_QUESTION_NOT_FOUND_ERROR("ASSIGNED_QUESTION_NOT_FOUND_ERROR", HttpStatus.NOT_FOUND, "해당 id를 가진 할당된 데일리 질문을 찾을 수 없습니다."),
     ANSWER_TIME_EXPIRED_ERROR("ANSWER_TIME_EXPIRED_ERROR", HttpStatus.FORBIDDEN, "오늘 할당된 질문에만 답변을 작성 및 수정할 수 있습니다."),
     ANSWER_NOT_FOUND_ERROR("ANSWER_NOT_FOUND_ERROR", HttpStatus.NOT_FOUND, "데일리 답변을 찾을 수 없습니다."),
+    ANSWER_ALREADY_EXIST_ERROR("ANSWER_ALREADY_EXIST_ERROR", HttpStatus.NOT_FOUND, "데일리 답변을 찾을 수 없습니다."),
     QNA_NOT_FOUND_ERROR("QNA_NOT_FOUND_ERROR", HttpStatus.INTERNAL_SERVER_ERROR, "질문 - 답변 데이터를 구성하는 중 문제가 발생했습니다.");
 
     private final String code;
